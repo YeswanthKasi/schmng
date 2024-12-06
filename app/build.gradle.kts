887f6820b28999dde7160427f7703a0f34d01f5b
Yeswanth <kasireddiyeswanth29@gmail.com>
2024-12-07 03:41:54 +0530

    Updated the project added CICD pipeline, added the test cases

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         create("release") {
             keyAlias = System.getenv("KEY_ALIAS") ?: "defaultAlias"
             keyPassword = System.getenv("KEY_PASSWORD") ?: "defaultKeyPassword"
-            storeFile = file(System.getenv("C:\\Users\\Yeswanth\\OneDrive\\Desktop\\schmng\\KEY_STORE\\schmng.jks") ?: "./app/KEY_STORE/schmng.jks") // Path to keystore
+            storeFile = file(System.getenv("KEYSTORE_PATH") ?: "KEY_STORE/schmng.jks")
             storePassword = System.getenv("STORE_PASSWORD") ?: "defaultStorePassword"
         }
     }
